442b082d1b807fbf44188c42529a46a6585568fb
Samonto Karmaker <samonto0726@gmail.com>
2026-08-24 14:37:22 +0600

    Loop exercise done

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,15 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum = 0;
+    for n in nums {
+        sum += n;
+    }
+    sum
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut vec = Vec::with_capacity(n);
+    for _ in 0..n {
+        vec.push(i);
+    }
+    vec
 }
