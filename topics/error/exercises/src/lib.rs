4ee46bf6902284f2aa9506b7ddf21798fbd89692
Samonto Karmaker <samonto0726@gmail.com>
2026-08-31 19:49:35 +0600

    Error handling exercise done

--- topics/error/exercises/src/lib.rs
+++ topics/error/exercises/src/lib.rs
@@ -4,9 +4,16 @@ pub enum MathError {
 }
 
 pub fn div(x: u32, y: u32) -> Result<u32, MathError> {
-    todo!();
+    if y == 0 {
+        return Err(MathError::DivByZero);
+    }
+    Ok(x / y)
 }
 
 pub fn get(v: &[u32], i: usize, default_val: u32) -> u32 {
-    todo!();
+    if i < v.len() {
+        v[i]
+    } else {
+        default_val
+    }
 }
